629ea4819f2986bda31a4f749fdf3f7c10c2f630
thoward@redhat.com <root@kvm.ocp.uluvus.hopto.org>
2021-03-23 16:10:00 +0700

    updated scripts

--- 00-label-nodes/01-label-nodes.sh
+++ 00-label-nodes/01-label-nodes.sh
@@ -6,5 +6,9 @@ oc label node infra0 node-role.kubernetes.io/infra=""
 oc label node infra1 node-role.kubernetes.io/infra=""
 oc label node infra2 node-role.kubernetes.io/infra=""
 
+oc label node infra0 node-role.kubernetes.io/router=""
+oc label node infra1 node-role.kubernetes.io/router=""
+oc label node infra2 node-role.kubernetes.io/router=""
 
+oc adm taint nodes -l node-role.kubernetes.io/infra infra=reserved:NoSchedule infra=reserved:NoExecute
 
