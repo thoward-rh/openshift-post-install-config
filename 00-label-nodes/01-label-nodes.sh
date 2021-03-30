e08e31782e6eac35a5226103cdaf8d77178ed24e
Tim Howard <66244275+thoward-rh@users.noreply.github.com>
2021-03-30 14:12:40 +0700

    Added logging node labels

--- 00-label-nodes/01-label-nodes.sh
+++ 00-label-nodes/01-label-nodes.sh
@@ -10,5 +10,9 @@ oc label node infra0 node-role.kubernetes.io/router=""
 oc label node infra1 node-role.kubernetes.io/router=""
 oc label node infra2 node-role.kubernetes.io/router=""
 
+oc label node infra0 node-role.kubernetes.io/logging=""
+oc label node infra1 node-role.kubernetes.io/logging=""
+oc label node infra2 node-role.kubernetes.io/logging=""
+
 oc adm taint nodes -l node-role.kubernetes.io/infra infra=reserved:NoSchedule infra=reserved:NoExecute
 
